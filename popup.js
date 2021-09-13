6e22f57a6c12efc0e956dc67c4797eb493f1a44d
tattee <tattee0626@gmail.com>
2021-09-13 20:24:21 +0900

    gateway2に対応

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-const urlSSLVPN = 'https://gateway.itc.u-tokyo.ac.jp/,DanaInfo='
+const urlSSLVPN = 'https://gateway2.itc.u-tokyo.ac.jp/,DanaInfo='
 chrome.tabs.query({ active: true, currentWindow: true }, (e) => {
     const url = e[0].url;
     const host = url.split('/')[2]
-    if (host.includes('gateway.itc.u-tokyo.ac.jp')){
+    if (host.includes('gateway2.itc.u-tokyo.ac.jp')){
         window.open(url, '_self')
     }else{
         const DanaInfo = `${host}+${url.split(host+'/')[1]}`
